b7c6734515f9e17419efd95d5e88733a9cc110e6
Danilka_Akarawita <112841934+Danilka-Akarawita@users.noreply.github.com>
2024-12-14 00:22:54 +0530

    inialized BusinessDetails

---
+++ app/api/businessDetails/[businessId]/route.tsx
@@ -0,0 +1,31 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+type Params = Promise<{ businessId: string }>;
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Params }
+) {
+  const { businessId } = await params;
+
+  console.log("Fetching businessId:", businessId);
+  try {
+    const businessListsDetailsById = await prisma.businessList.findMany({
+      where: {
+        id:parseInt(businessId, 10)
+      },
+      include: {
+        category: true, 
+      },
+    });
+
+    return NextResponse.json(businessListsDetailsById, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching businessListsDetails:", error);
+
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
